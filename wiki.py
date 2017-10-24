6f3c3e88dfaca1c2fec7d43f1751b598dedb5d89
Rainer Keller <mail@rainerkeller.de>
2017-10-24 20:14:54 +0200

    Remove javascript

--- wiki.py
+++ wiki.py
@@ -6,7 +6,7 @@ import git
 import mimetypes
 import configparser
 import os
-import json
+import cgi
 from urllib.parse import urlparse
 
 class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
@@ -53,62 +53,92 @@ class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
         url, path = self.validatedPath()
 
-        if self.repo.bare or url.query != "commit":
+        if self.repo.bare:
             self.send_response(403)
             self.end_headers()
             return
 
-        content = self.rfile.read(int(self.headers['Content-Length']))
+        form = cgi.FieldStorage(fp = self.rfile, headers = self.headers,
+                environ={'REQUEST_METHOD':'POST',
+                         'CONTENT_TYPE':self.headers['Content-Type'],
+                     })
 
-        with open(config.get('Wiki', 'Repository') + "/" + path, "wb") as f:
-            f.write(content)
+        if "textarea" in form:
+            with open(config.get('Wiki', 'Repository') + "/" + path, "wb") as f:
+                f.write(form.getvalue('textarea').encode('utf8'))
 
         self.repo.index.add([path])
         self.repo.index.commit("Commit message", author=git.Actor("Author name", "author@example.com"), committer=git.Actor("Committer name", "committer@example.com"))
-        self.send_response(200)
-        self.end_headers()
+
+        self.do_GET()
         return
 
     def do_GET(self):
         self.initRepo()
+        template = None
+        output = None
+        stylesheets = ""
 
         url, path = self.validatedPath()
 
         try:
-            text = self.getContentsFromGit(path)
+            output = self.getContentsFromGit(path)
         except Exception as e:
-            self.send_response(404)
-            self.end_headers()
+            template = "create"
             print(e)
-            return
 
         contentType, encoding = mimetypes.guess_type(path)
-        if not contentType:
-            if url.query == 'raw':
-                contentType = 'text/plain'
-            elif url.query.startswith('search='):
-                result = self.searchRepo(url.query[7:])
-                contentType = 'application/json'
-                text = json.dumps(result).encode('utf8')
+
+        if contentType and template == "create":
+            self.send_response(404)
+            self.end_headers()
+            return
+
+        if not template:
+            if url.query == "edit":
+                template = "edit"
+            elif url.query.startswith("search="):
+                template = "search"
             else:
-                contentType == 'text/html'
-                with open('wiki.html', 'r') as f:
-                    template = f.read()
-                    template = template.replace("@TITLE@", config.get('Wiki', 'Title', fallback="<no title>"))
-                    html = self.renderHTML(text)
-                    if self.repo.bare:
-                        html = '<script>document.getElementById("editButton").classList.add("hidden")</script>' + html
-                    template = template.replace("@HTML_HERE@", html)
-                    if config.has_option('Wiki', 'Stylesheet'):
-                        template = template.replace("@STYLESHEETS@", '<link rel="stylesheet" type="text/css" href="%s" />' % config.get('Wiki', 'Stylesheet'))
-                    else:
-                        template = template.replace("@STYLESHEETS@", "")
-                    text = template.encode('utf8')
+                template = "view"
+
+        if not contentType:
+            content = None
+            contentType == 'text/html'
+
+            if template == 'edit':
+                content = output.decode('utf8')
+            elif template == "search":
+                content = ''
+                results = self.searchRepo(url.query[7:])
+                if len(results):
+                    content = content + "Search results for: " + url.query[7:]
+                else:
+                    content = content + "No results for: " + url.query[7:]
+
+                for i in results:
+                    content = content + ('<div><a href="%s"/>%s</a><div>%s</div></div>' % (i['filename'], i['filename'], i['text']))
+            else:
+                content = self.renderHTML(output)
+
+            if self.repo.bare:
+                stylesheets = stylesheets + "<style>.edit { display:none; }</style>"
+            if config.has_option('Wiki', 'Stylesheet'):
+                stylesheets = stylesheets + ('<link rel="stylesheet" type="text/css" href="%s" />' % config.get('Wiki', 'Stylesheet'))
+
+            with open(template + ".html" , 'r') as f:
+                output = f.read()
+            output = output.replace("@EDIT_LINK@", url.path + "?edit")
+            output = output.replace("@SAVE_LINK@", url.path)
+            output = output.replace("@CONTENT@", content)
+            output = output.replace("@TITLE@", config.get('Wiki', 'Title', fallback="<no title>"))
+            output = output.replace("@STYLESHEETS@", stylesheets)
+            output = output.encode('utf8')
 
         self.send_response(200)
         self.send_header('Content-type', contentType)
         self.end_headers()
-        self.wfile.write(text)
+        self.wfile.write(output)
         return
 
 config = configparser.ConfigParser()
